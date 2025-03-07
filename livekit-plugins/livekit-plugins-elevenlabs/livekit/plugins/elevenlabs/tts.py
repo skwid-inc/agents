c4d4dd1fb1a8898d883c6a1d463968a915b28cf6
smarakp <smarak@trysalient.com>
2025-03-07 11:33:55 -0800

    logging:

--- livekit-plugins/livekit-plugins-elevenlabs/livekit/plugins/elevenlabs/tts.py
+++ livekit-plugins/livekit-plugins-elevenlabs/livekit/plugins/elevenlabs/tts.py
@@ -571,6 +571,7 @@ class SynthesizeStream(tts.SynthesizeStream):
 
                 while True:
                     msg = await ws_conn.receive()
+                    logger.info(f"Received message from 11labs: {msg}")
                     if msg.type in (
                         aiohttp.WSMsgType.CLOSED,
                         aiohttp.WSMsgType.CLOSE,
